81c5486b77a08d46d98e5128145cd1b1c1399eea
ivan <ivsound93@gmail.com>
2022-02-24 23:06:42 +0300

    Long Adder

--- src/main/java/ru/csc/java2014/multithreading/demo3/Account.java
+++ src/main/java/ru/csc/java2014/multithreading/demo3/Account.java
@@ -1,35 +1,70 @@
 package ru.csc.java2014.multithreading.demo3;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Account {
 
-  //    private /*volatile*/ long balance;
-  private final AtomicLong balance;
+  private final LongAdder adder;
+//  private final AtomicLong balance;
 
-  public Account() {
-    this(new AtomicLong(0));
+//  public Account() {
+//    this(new AtomicLong(0));
+//  }
+
+//    public Account() {
+//    this(0);
+//  }
+//
+//  public Account(AtomicLong balance) {
+//    this.balance = balance;
+//  }
+//
+//  public AtomicLong getBalance() {
+//    return balance;
+//  }
+//
+//  public void deposit(long amount) {
+//    checkAmountNonNegative(amount);
+//    balance.getAndAdd(amount);
+//  }
+//
+//  public void withdraw(long amount) {
+//    checkAmountNonNegative(amount);
+//    if (balance.get() < amount) {
+//      throw new IllegalArgumentException("not enough money");
+//    }
+//    balance.getAndAdd(-amount);
+//  }
+//
+//  private static void checkAmountNonNegative(long amount) {
+//    if (amount < 0) {
+//      throw new IllegalArgumentException("negative amount");
+//    }
+//  }
+
+  public Account(long balance) {
+    adder = new LongAdder();
+    adder.add(balance);
   }
 
-  public Account(AtomicLong balance) {
-    this.balance = balance;
+  public long getBalance() {
+    return adder.longValue();
   }
 
-  public AtomicLong getBalance() {
-    return balance;
-  }
-
-  public void deposit(long amount) {
+  public void deposit(int amount) {
     checkAmountNonNegative(amount);
-    balance.getAndAdd(amount);
+    adder.add(amount);
+    adder.sum();
+
   }
 
-  public void withdraw(long amount) {
+  public void withdraw(int amount) {
     checkAmountNonNegative(amount);
-    if (balance.get() < amount) {
+    if (adder.longValue() < amount) {
       throw new IllegalArgumentException("not enough money");
     }
-    balance.getAndAdd(-amount);
+    adder.add(-amount);
+    adder.sum();
   }
 
   private static void checkAmountNonNegative(long amount) {
--- src/main/java/ru/csc/java2014/multithreading/demo3/Demo3.java
+++ src/main/java/ru/csc/java2014/multithreading/demo3/Demo3.java
@@ -1,23 +1,23 @@
 package ru.csc.java2014.multithreading.demo3;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 public class Demo3 {
 
   public static void main(String[] args) throws Exception {
-    Account account = new Account(new AtomicLong(100000));
+
+    Account account = new Account(100000);
+
     System.out.println("Begin balance = " + account.getBalance());
 
     Runnable r1 = () -> { // anonymous class
-        for (int i = 0; i < 20000; ++i) {
-          account.withdraw(1);
-        }
+      for (int i = 0; i < 20000; ++i) {
+        account.withdraw(1);
+      }
     };
 
     Runnable r2 = () -> { // anonymous class
-        for (int i = 0; i < 20000; ++i) {
-          account.deposit(1);
-        }
+      for (int i = 0; i < 20000; ++i) {
+        account.deposit(1);
+      }
     };
 
     Thread withdrawThread = new Thread(r1);
